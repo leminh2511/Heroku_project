9c66b4e06ac8fc9fd9861495b285cdad5ed155dd
PC\Administrator <bibibobo2511@gmail.com>
2016-12-17 04:37:08 +0700

    Update_Form_Sport_Day8

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,request
+import mongoengine
+from mongoengine import *
 
 app = Flask(__name__)
-
-
+host="ds133428.mlab.com"
+port=33428
+db_name="minhle"
+user_name="sport_minh"
+password="ckiuckiu2511"
+mongoengine.connect(db_name, host=host, port=port, username=user_name, password=password)
+class User(Document):
+    email=StringField()
+    password=StringField()
+class Sport(Document):
+    name=StringField()
+    sport=StringField()
+    link=StringField()
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -42,15 +55,45 @@ sport_list=[
         "Desc":" International Boxing Federation",
         "link":"http://www.worldboxingfederation.net/",
         "img":"https://s-media-cache-ak0.pinimg.com/736x/d1/10/71/d110715ac88f2caaf3c09a3cd5befa01.jpg"
+    },
+    {
+        "Name":"Japanese Anti-Virus",
+        "Desc":"JAV ^^",
+        "link":"https://www.facebook.com/javfanclub02/",
+        "img":"http://www.japantrends.com/japan-trends/wp-content/uploads/2014/08/akb48-idol-group-decline-popularity-tv-commercials.jpg"
     }
 ]
-@app.route("/sport")
+@app.route("/sport",methods=["GET","POST"])
 def sport():
-    return render_template("Sport.html",sport_list=sport_list)
+    if request.method =="GET":
+        return render_template("Sport.html",sport_list=sport_list)
+    elif request.method=="POST":
+        name=request.form["name"]
+        sport=request.form["sport"]
+        link=request.form["link"]
+
+        idea=Sport(name=name,sport=sport,link=link)
+        idea.save()
+        return "Thank for your ideas"
 
 @app.route("/myself")
 def myself():
     return render_template("my_self.html")
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.method=="GET":
+        return render_template("register_form.html")
+    elif request.method=="POST":
+        email=request.form["email"]
+        password=request.form["password"]
+
+        user = User(email=email,password=password) #Contructor
+        user.save()
+        # kiem tra tai khoan
+        return "Thank you for registering"
+
+
 
 if __name__ == '__main__':
     app.run()
